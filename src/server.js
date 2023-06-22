7e4e154aee487a90bc2b9ad85f05a0d54afff630
tablonimus <lautamartin1995@gmail.com>
2023-06-21 23:10:20 -0300

    agregado nodemon

--- src/server.js
+++ src/server.js
@@ -15,7 +15,13 @@ server.get("/", (req,res)=>{
 
 
 
-
+server.use((err, req, res, next) => {
+    // eslint-disable-line no-unused-vars
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.error(err);
+    res.status(status).send(message);
+  });
 
 
 module.exports = server;
